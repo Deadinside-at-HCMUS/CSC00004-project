c0a0ddd61d55a8d2eb28fc8714d43027e38d1234
Tointech <leanhthunk@gmail.com>
2021-11-30 22:20:54 +0700

    update total cart value

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,14 @@
+// Update cart
+function updatecart() 
+{
+  var price = document.getElementById("ticket_price").innerHTML;
+  var quantity = document.getElementById("num").value;
+  var total = parseFloat(price) * quantity
+  if (!isNaN(total))
+    document.getElementById("total").innerHTML = total
+    document.getElementById("total-cart").innerHTML = total
+}
+
 // Delete cart
 var remove_cart = document.getElementsByClassName("btn-danger");
 for (var i = 0; i < remove_cart.length; i++) {
@@ -5,5 +16,6 @@ for (var i = 0; i < remove_cart.length; i++) {
   button.addEventListener("click", function () {
     var button_remove = event.target
     button_remove.parentElement.parentElement.remove()
+    document.getElementById("total-cart").innerHTML = 0
   })
 }
